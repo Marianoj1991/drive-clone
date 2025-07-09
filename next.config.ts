ef4c0cc97885ca518b1a4b68f6eda4117a78aa18
Mariano Jimenez <104471721+Marianoj1991@users.noreply.github.com>
2025-07-09 18:31:34 -0300

    [fix] Deploy problems

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: false
+  },
+  eslint: {
+    ignoreDuringBuilds: true
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: '100MB'
